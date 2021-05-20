d56c6c4a93681bd4d9d624c50711d72cf62906e7
Jesus Ramirez <jrmz.maldonado@gmail.com>
2021-05-20 17:16:21 -0500

    Update README and remove contact form functionality

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# webpage
-Personal Webpage
+# Old Portfolio
+
+This repo belongs to my old personal portfolio, before 2019.
+
+The site doesn't use any fancy things, it's pure HTML, CSS, and JS.
--- query.js
+++ query.js
@@ -4,15 +4,15 @@ const chooseAbout = document.querySelector('#aboutNav');
 const myMenu = document.querySelector('#myMenu');
 const toAbout = document.querySelector('#toAbout');
 
-function aboutIntoView(){
-    console.log('slow');
-    var about = document.getElementById('about');
-    about.scrollIntoView({behavior:'smooth'});
+function aboutIntoView() {
+  console.log('slow');
+  var about = document.getElementById('about');
+  about.scrollIntoView({ behavior: 'smooth' });
 }
 
-function openMenu(){
-    console.log('open');
-    myMenu.classList.toggle('open');
+function openMenu() {
+  console.log('open');
+  myMenu.classList.toggle('open');
 }
 
 toAbout.addEventListener('click', aboutIntoView);
@@ -20,46 +20,32 @@ menuBtn.addEventListener('click', openMenu);
 closeMenuBtn.addEventListener('click', openMenu);
 chooseAbout.addEventListener('click', openMenu);
 
-// Code below for the Contact Page Form 
+// Code below for the Contact Page Form
 //form="contactMe"
 
 const submitBtn = document.querySelector('.submitBtn');
-const regexpEmail = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+const regexpEmail =
+  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 const inputs = document.querySelectorAll('.nameInput');
-function sendEmail(){
-    var form = document.getElementById('contactMe');
-    var att = document.createAttribute('action');
-    att.value = 'first.php';
-    form.setAttributeNode(att);
-    att = document.createAttribute('method');
-    att.value='post';
-    form.setAttributeNode(att);
-    att = document.createAttribute('form');
-    att.value = 'contactMe';
-    submitBtn.setAttributeNode(att);
-    console.log(form);
-    console.log(submitBtn);
+function moveLabel() {
+  console.log('focus IN');
+  console.log(this);
 }
-function moveLabel(){
-    console.log('focus IN');
-    console.log(this);
-}
-function checkForm(){ 
-    var errorForm = document.getElementById('errorForm');
-    var errorEmail = document.getElementById('errorEmail');
-    if(!formName.value || !formEmail.value || !formMessage.value){
-        errorForm.style.display = 'block';
-        errorEmail.style.display = 'none';
-    }else{
-        errorForm.style.display = 'none';
-        var validEmail = regexpEmail.test(formEmail.value);
-        if(!validEmail){
-            errorEmail.style.display = 'block';
-        }
-        submitBtn.innerHTML = 'Sending...';
-        sendEmail();
+function checkForm() {
+  var errorForm = document.getElementById('errorForm');
+  var errorEmail = document.getElementById('errorEmail');
+  if (!formName.value || !formEmail.value || !formMessage.value) {
+    errorForm.style.display = 'block';
+    errorEmail.style.display = 'none';
+  } else {
+    errorForm.style.display = 'none';
+    var validEmail = regexpEmail.test(formEmail.value);
+    if (!validEmail) {
+      errorEmail.style.display = 'block';
     }
+    submitBtn.innerHTML = 'Sending...';
+    sendEmail();
+  }
 }
-inputs.forEach(input => input.addEventListener('focus', moveLabel));
+inputs.forEach((input) => input.addEventListener('focus', moveLabel));
 submitBtn.addEventListener('click', checkForm);
-
